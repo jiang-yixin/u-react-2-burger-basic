64ac9c7dbe4e37bb83e02faf617c2c812ddf4cf7
Yixin Jiang <jiangyixin0910@gmail.com>
2021-01-08 22:56:20 +0100

    Add page orders

---
+++ src/components/Order/Order.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import classes from './Order.css';
+
+const order = (props) => {
+    const ingredientsOutput = Object.keys(props.ingredients).map(igKey => (
+        <span style={{
+            textTransform: 'capitalize',
+            display: 'inline-block',
+            margin: '0 8px',
+            border: '1px solid #ccc',
+            padding: '5px'
+        }}>{igKey} ({props.ingredients[igKey]})</span>
+    ));
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientsOutput}</p>
+            <p>Price: <strong>USD {props.price}</strong></p>
+        </div>
+    );
+};
+
+export default order;
